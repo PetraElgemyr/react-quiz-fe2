f280a1ebeca84dd5f664a24cf0bd93f500fa5370
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-14 22:34:45 +0100

    fix interfaces for questions and answers

--- src/components/interfaces/IQuestion.ts
+++ src/components/interfaces/IQuestion.ts
@@ -1,9 +1,4 @@
 export interface IQuestion {
-  //     "id": 1,
-  //     "question": "Vilket av dessa år var ett skottår?",
-  //     "options": [2002, 2004, 2006],
-  //     "answer": 2004
-  //   },
   id: number;
   question: string;
   options: string[];
