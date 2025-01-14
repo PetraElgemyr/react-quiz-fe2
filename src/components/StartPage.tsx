885ac50289f3a56d90ea9fd14c262d929188aacb
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-14 19:26:21 +0100

    add title

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -6,6 +6,7 @@ export const StartPage = () => {
   return (
     <>
       <h1>Quizdags!</h1>
+      <h3>Är du smartaren än en ph-deltagare? Dags att ta reda på det!</h3>
       <button onClick={() => navigate("/game")}>Starta quiz</button>
     </>
   );
