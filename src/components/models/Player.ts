c4c3ac000b74cf0d06bd134d93a4525becdcb097
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-15 14:14:51 +0100

    add default player

--- src/components/models/Player.ts
+++ src/components/models/Player.ts
@@ -1,11 +1,13 @@
 import { IAnswer } from "../interfaces/IAnswer";
+import { v4 as uuidv4 } from "uuid";
 
 export class Player {
   constructor(
+    public id: string,
     public name: string,
     public score: number,
     public answers: IAnswer[]
   ) {}
 }
 
-export const defaultEmptyPlayer = new Player("", 0, []);
+export const defaultEmptyPlayer = new Player(uuidv4().toString(), "", 0, []);
