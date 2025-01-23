e8b470cdad7d3769174dbe4ade0b45e73aaccc95
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-23 15:02:43 +0100

    add variables

---
+++ src/styled/Variables/Breakpoints.tsx
@@ -0,0 +1,9 @@
+export const Breakpoints = {
+  miniMobileBreakpoint: 200,
+  mobileSBreakpoint: 320,
+  mobileLBreakpoint: 425,
+  tabletBreakpoint: 768,
+  laptopBreakpoint: 1024,
+  desktopBreakpoint: 1440,
+  fourKBreakpoint: 2560,
+};
