fb8293326e6c41f58c8e3c51e5478b845ad58244
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-26 22:34:30 +0100

    add color variable

--- src/styled/Variables/Colors.tsx
+++ src/styled/Variables/Colors.tsx
@@ -1,6 +1,7 @@
 export const Colors = {
   primaryFrostyWhite: "#ffffff3b",
   secondaryFrostyWhite: "#e1bed04a",
+  backgroundWhite: "rgba(249, 249, 249, 0.85)",
   primaryGold:
     "radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%,rgb(237 197 117) 8%, #b38b35 30%, #9b7e3b 40%, transparent 80%), radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%,rgb(255, 243, 172) 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)",
 };
