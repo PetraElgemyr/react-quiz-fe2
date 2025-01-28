e8d8923be3b2c7945e99643e268424f568cd273f
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-28 17:46:22 +0100

    fix styling on card options

--- src/components/themes/QuestionOptionCardTheme.tsx
+++ src/components/themes/QuestionOptionCardTheme.tsx
@@ -4,15 +4,12 @@ import { Colors } from "../../styled/Variables/Colors";
 export const QuestionOptionCardTheme = createTheme({
   components: {
     MuiCard: {
-      defaultProps: {
-        sx: {
+      styleOverrides: {
+        root: {
           textAlign: "center",
           margin: 0,
           padding: 0,
           backgroundColor: Colors.backgroundWhite,
-          ":hover": {
-            boxShadow: "inset 0 0 8px rgb(46, 45, 45)",
-          },
         },
       },
     },
