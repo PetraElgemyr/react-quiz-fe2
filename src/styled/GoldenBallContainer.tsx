f4a60d48bc37e00a0f4b31e8124ec9a29471b85a
PetraElgemyr <petra.elgemyr@gmail.com>
2025-01-28 17:46:01 +0100

    adjust golden ball

--- src/styled/GoldenBallContainer.tsx
+++ src/styled/GoldenBallContainer.tsx
@@ -3,16 +3,18 @@ import { devices } from "./Variables/Devices";
 
 export const GoldenBallContainer = styled.div`
   width: 35%;
+  padding: 0;
+  margin: 0;
 
   @media screen and (${devices.tablet}) {
-    width: 20%;
-  }
-
-  @media screen and (${devices.laptop}) {
     width: 15%;
   }
 
+  @media screen and (${devices.laptop}) {
+    width: 12%;
+  }
+
   @media screen and (${devices.laptopL}) {
-    width: 10%;
+    width: 8%;
   }
 `;
--- src/styled/LogoContainer.tsx
+++ src/styled/LogoContainer.tsx
@@ -6,7 +6,7 @@ export const LogoContainer = styled.div`
   margin: 0;
   display: flex;
   justify-content: center;
-  padding: 1% 3% 3% 4%;
+  padding: 0 3% 3% 4%;
 
   &:hover {
     cursor: pointer;
@@ -15,19 +15,4 @@ export const LogoContainer = styled.div`
   @media screen and (${devices.mobileL}) {
     width: 70%;
   }
-
-  // @media screen and (${devices.tablet}) {
-  //   width: 50%;
-  //   padding: 0 1% 4% 2%;
-  // }
-
-  @media screen and (${devices.tablet}) {
-    width: 100%;
-    padding: 0.5% 3% 2% 4%;
-  }
-
-  // @media screen and (${devices.laptop}) {
-  //   width: 100%;
-  //   padding: 0.5% 3% 2% 4%;
-  // }
 `;
